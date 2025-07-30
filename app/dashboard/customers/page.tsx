fc74f0e199b76f5f8bbcc5ac3335211070201312
Vlad Demchuk <vladyslavdemchuk1999@gmail.com>
2025-07-30 18:13:44 +0300

    Refactor dashboard components and add data fetching for cards and invoices

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,4 +1,6 @@
-export default function CustomersPage() {
+import { fetchRevenue } from '@/app/lib/data';
+
+export default async function CustomersPage() {
   return (
     <p>Customers Page</p>
   );
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -17,7 +17,7 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
@@ -27,6 +27,18 @@ export async function fetchRevenue() {
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch revenue data.');
+  }
+}
+
+export async function getInvoicesCount() {
+  try {
+    const data = await sql`
+    SELECT COUNT(*) FROM invoices` ;
+
+    return data
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to fetch count of invoices.');
   }
 }
 
